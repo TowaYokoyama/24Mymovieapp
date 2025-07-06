760356834fbe9e9bc64a963aa14fde5c3046d84e
Towa Yokoyama <soccertowa0923@outlook.com>
2025-07-07 08:42:01 +0900

    appwriteの編集

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -4,8 +4,9 @@ import {Client, Databases, Query, ID} from 'react-native-appwrite'
 const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
+const ENDPOINT = process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!;
 const client = new Client()
-.setEndpoint('https://cloud.appwrite.io/v1')
+.setEndpoint(ENDPOINT) //ここが問題で間違っていることがわかる
 .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
 
 
@@ -37,6 +38,7 @@ export const updateSearchCount = async(query: string, movie : Movie) => {
             searchTerm: query,
             movie_id: movie.id,
             count: 1,
+            title: movie.title,
             poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
         });
     }
